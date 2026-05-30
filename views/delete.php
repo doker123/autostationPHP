ab55eeae54a1d37d81202e236f8237a0f4545ad5
doker123 <dodo2larin@gmail.com>
2026-05-29 22:55:19 -0700

    Сделал редирект в удаленнии записи

--- views/delete.php
+++ views/delete.php
@@ -1,4 +1,8 @@
 <?php
+$ROOT_PATH = __DIR__;
+$parts = explode('/', trim($ROOT_PATH, '/'));
+$pathRoot = '/'.$parts[4].'/';
+
 $parking_id = $id ?? '';
 $errors = [];
 $record = null;
@@ -9,7 +13,15 @@ try {
     $sql = "";
 
 
+
+    $pdo->commit();
 }catch (Exception $e){
+    if (isset($pdo) && $pdo->inTransaction()) {
+        $pdo->rollBack();
+    }
     $errors[] = 'Ошибка базы данных' . $e->getMessage();
 }
+
+header("Location: ". $pathRoot ."home.php");
+exit();
 ?>
